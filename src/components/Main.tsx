358b6263c65916a4b37763304df207918a2f8504
seohan1010 <seohan1010>
2024-02-18 19:52:16 +0900

    calculator input test complete

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import classes from '../style/main.module.css';
 
-
 const Main = ()=>{
+    const [input,setInput] =useState<string>('')
 
+
+const onClickHandler=()=>{
+        setInput((prev)=>prev + " new value ")
+}
 
 return (
 
 <>
 <div className={classes.main}>
-    <div className={classes.top_blank}></div>
-
+    <input className={classes.top_blank}  type='text' readOnly={true} value={input}></input>
+    
+    {/* i have to  make some grid for it*/}
+    <button onClick={()=>onClickHandler()}> test button</button>
 
 </div>
 </>
