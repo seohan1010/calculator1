325b660b1ff5daea9ccf5eecd5d75a74adeb4aed
seohan1010 <seohan1010>
2024-02-18 19:32:16 +0900

    initial commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,22 @@
+import classes from '../style/main.module.css';
+
+
+const Main = ()=>{
+
+
+return (
+
+<>
+<div className={classes.main}>
+    <div className={classes.top_blank}></div>
+
+
+</div>
+</>
+
+)
+
+}
+
+
+export default Main;
